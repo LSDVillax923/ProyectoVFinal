733f75d4beaf15937efe51051a6eb7fcf81ffc5d
LSD_Villax <andresvillabon@gmail.com>
2026-05-19 14:47:21 -0500

    puesrt

--- front/FrontMediCat/src/app/shared/api/rest-endpoints.ts
+++ front/FrontMediCat/src/app/shared/api/rest-endpoints.ts
@@ -1,8 +1,8 @@
 // ============================================
 // CONFIGURACIÓN BASE
 // ============================================
-export const API_BASE_URL = 'http://localhost:8081/api';
-export const STATIC_BASE_URL = 'http://localhost:8081';
+export const API_BASE_URL = 'http://localhost:8080/api';
+export const STATIC_BASE_URL = 'http://localhost:8080';
 export const MASCOTAS_FOTO_BASE = `${STATIC_BASE_URL}/img`;
 
 // ============================================
